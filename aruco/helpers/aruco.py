2b4be4df11c4b7a48ab02ff0fb34b00f0f797c11
Pedro Henrique Cordeiro Soares <phcs.971@gmail.com>
2023-10-05 19:50:03 -0300

    finish aruco tracker

--- aruco/helpers/aruco.py
+++ aruco/helpers/aruco.py
@@ -1,10 +1,12 @@
 import cv2
 import cv2.aruco as aruco
+from helpers.tracker import Tracker
 
 class ArucoDetector:
-    def __init__(self):
+    def __init__(self, tracker: Tracker):
         self.dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_100)
         self.params = aruco.DetectorParameters()
+        self.tracker = tracker
 
     def detect_markers(self, image):
         (corners, ids, rejected) = cv2.aruco.detectMarkers(image, self.dict, parameters=self.params)
@@ -17,25 +19,26 @@ class ArucoDetector:
                 # top-left, top-right, bottom-right, and bottom-left order)
                 corners = markerCorner.reshape((4, 2))
                 (topLeft, topRight, bottomRight, bottomLeft) = corners
+
                 # convert each of the (x, y)-coordinate pairs to integers
                 topRight = (int(topRight[0]), int(topRight[1]))
                 bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
                 bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
                 topLeft = (int(topLeft[0]), int(topLeft[1]))
-                		# draw the bounding box of the ArUCo detection
-                cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-                cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-                cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-                cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
-                # compute and draw the center (x, y)-coordinates of the ArUco
-                # marker
+
+                # compute and draw the center (x, y)-coordinates of the ArUco marker
                 cX = int((topLeft[0] + bottomRight[0]) / 2.0)
                 cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-                cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
-                # draw the ArUco marker ID on the image
-                cv2.putText(image, str(markerID),
-                    (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, (0, 255, 0), 2)
+                color = self.tracker.add(markerID, cX, cY).color
+
+                # draw the bounding box of the ArUCo detection
+                cv2.line(image, topLeft, topRight, color, 2)
+                cv2.line(image, topRight, bottomRight, color, 2)
+                cv2.line(image, bottomRight, bottomLeft, color, 2)
+                cv2.line(image, bottomLeft, topLeft, color, 2)
+                
+                cv2.circle(image, (cX, cY), 4, color, -1)
+                
                 print("[INFO] ArUco marker ID: {}".format(markerID))
         return image
 
--- aruco/main.py
+++ aruco/main.py
@@ -1,16 +1,42 @@
 from helpers.webcam import *
 from helpers.aruco import *
+from helpers.tracker import Tracker
+from time import time
 
 webcam = WebcamVideoStream()
-detector = ArucoDetector()
+tracker = Tracker()
+detector = ArucoDetector(tracker)
 
-if __name__ == '__main__':
+
+def main():
     while True:
         frame = webcam.get_frame()
         frame = detector.detect_markers(frame)
-        cv2.imshow('frame', cv2.flip(frame, 1))
+        frame = tracker.draw(frame)
+        frame = cv2.flip(frame, 1)
+        webcam.fps(frame)
+
+        cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    save()
+
+def save():
+    frame = webcam.get_blank_image()
+    frame = tracker.draw(frame, all_positions=True)
+    frame = cv2.flip(frame, 1)
+    cv2.imwrite(f"output/{int(time())}.png", frame)
+
+
+def close():
     webcam.__del__()
     cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        close()
